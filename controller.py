722dd675e8cda9ecae5ad33eabce0b2d961272fe
Eurivaldo Filho <eurivaldojvf@gmail.com>
2023-05-23 12:11:54 -0300

    Add CRUD controllers

--- controller.py
+++ controller.py
@@ -1,32 +1,32 @@
 from constants import DEFAULT_COMMA_CODE
-
-class TransactionController:
+class TransactionController: 
   transactions = []
-  
+
   def get_all_from_db(self):
-      with open('transactions.csv') as data:
-        db_data = data.read()
+    with open('transactions.csv') as data:
+      db_data = data.read()
 
-        db_data_array = db_data.split(',')
-        transactions = []
+      db_data_array = db_data.split(',')
+      transactions = []
 
-        for db_transaction in db_data_array:
-          transaction = {}
-          # Normalizando as víruglas para evitar que conflitem com csv
-          normalized_string = db_transaction.replace(DEFAULT_COMMA_CODE, ',')
-          
-          # Removendo as divisões entre chaves e valores
-          splitted_values = normalized_string.split('||')
-          
-          for without_pipes_string in splitted_values:
-            # unpacking em dois valores do array 'chave:valor'
-            key, value = without_pipes_string.split(':')
+      for db_transaction in db_data_array:
+        transaction = {}
+        # Normalizando as víruglas para evitar que conflitem com csv
+        normalized_string = db_transaction.replace(DEFAULT_COMMA_CODE, ',')
+        
+        # Removendo as divisões entre chaves e valores
+        splitted_values = normalized_string.split(';')
 
-            transaction[key] = value
+        for without_pipes_string in splitted_values:
+          # unpacking em dois valores do array 'chave:valor'
+          print(without_pipes_string)
+          key, value = without_pipes_string.split(':')
 
-          transactions.append(transaction)
+          transaction[key] = value
 
-        self.transactions = transactions
+        transactions.append(transaction)
+
+      self.transactions = transactions
 
   def add_transaction_in_db(self, transaction):
     last_transaction = self.transactions[-1]
@@ -40,7 +40,7 @@ class TransactionController:
     for index, key_and_value_tuple in enumerate(transaction_items):
       key, value = key_and_value_tuple
       is_last_item = index == len(transaction_items) - 1
-      parsed_keys_and_values.append(f'{key}:{value}'if is_last_item  else f'{key}:{value}||')
+      parsed_keys_and_values.append(f'{key}:{value}'if is_last_item  else f'{key}:{value};')
     
     transaction_string = ',' + ''.join(parsed_keys_and_values)
 
@@ -48,6 +48,53 @@ class TransactionController:
     db.write(transaction_string)
     db.close()
     self.get_all_from_db()
+
+  def get_transaction_by_id(self, id):
+    for transaction in self.transactions:
+      if transaction['id'] == id:
+        print(transaction)
+        return transaction
+    
+  def delete_transaction_by_id(self, id):
+    transaction = self.get_transaction_by_id(id)
+    transaction_index = self.transactions.index(transaction)
+    print(transaction_index)
+    del self.transactions[transaction_index]
+
+    self.update_db()
+
+  def update_transaction_by_id(self, id, new_transaction):
+    transaction = self.get_transaction_by_id(id)
+    transaction_index = self.transactions.index(transaction)
+
+    self.transactions[transaction_index] = new_transaction
+
+    self.update_db()
+
+  def update_db(self):
+    db = open('transactions.csv', 'w')
+    db.write('')
+    db.close()
+
+    csv = ''
+
+    for transaction in self.transactions:
+      parsed = self.parse_to_csv(transaction)
+      csv += parsed + ',' if transaction != self.transactions[-1] else parsed
+    
+    db = open('transactions.csv', 'a')
+    db.write(csv)
+
+  def parse_to_csv(self, transaction):
+    parsed_keys_and_values = []
+    transaction_items = transaction.items()
+
+    for index, key_and_value_tuple in enumerate(transaction_items):
+      key, value = key_and_value_tuple
+      is_last_item = index == len(transaction_items) - 1
+      parsed_keys_and_values.append(f'{key}:{value}'if is_last_item  else f'{key}:{value};')
+    
+    return ''.join(parsed_keys_and_values)
 
   def __init__(self):
     self.get_all_from_db()
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from controller import TransactionController
 
-print(TransactionController().transactions)
+print("Bem vindo, ao seu gerenciador de transações!")
+
+TransactionController.delete_transaction_by_id(TransactionController(), '5')
